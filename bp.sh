bb71fac34774153f17cfea8116f581e6804cf0bb
= <neosam777@gmail.com>
2023-01-31 21:21:55 +0100

    build

---
+++ bp.sh
@@ -0,0 +1,8 @@
+ng build --output-path docs --base-href https://neoisthere.github.io/webdev-consulting-firm/
+cd ./docs
+copy index.html 404.html
+cd ..
+git add .
+git commit -m"build"
+git push origin dev
+
