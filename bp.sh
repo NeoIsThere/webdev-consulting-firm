3c54097f5d810bd1b8c9304277b6de0a283c4013
= <neosam777@gmail.com>
2023-03-11 14:48:36 +0100

    untracked script file

--- bp.sh
+++
@@ -1,8 +0,0 @@
-ng build --output-path docs --base-href https://neoisthere.github.io/webdev-consulting-firm/
-cd ./docs
-cp index.html 404.html
-cd ..
-git add .
-git commit -m"build"
-git push origin prod
-
